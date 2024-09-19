882ddb028a2701d3fbfe36e5ded69ab2b5d5fde2
Santiago Gutierrez Villegas <santiago.gutierrez01@globant.com>
2024-09-18 20:43:54 -0500

    Chore: Do some changes to CI pineline and tests

--- src/test/java/com/globant/utils/drivers/WebDriverFactory.java
+++ src/test/java/com/globant/utils/drivers/WebDriverFactory.java
@@ -4,14 +4,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class WebDriverFactory {
+
     public static WebDriver createDriver() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        return new ChromeDriver(options);
+        String browserName = System.getProperty("browserName", "chrome").toLowerCase();
+
+        switch (browserName) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--no-sandbox");
+                firefoxOptions.addArguments("--disable-dev-shm-usage");
+                return new FirefoxDriver(firefoxOptions);
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless");
+                edgeOptions.addArguments("--no-sandbox");
+                edgeOptions.addArguments("--disable-dev-shm-usage");
+                return new EdgeDriver(edgeOptions);
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                return new ChromeDriver(chromeOptions);
+        }
     }
 }
